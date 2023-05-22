5a19d3b69b8671b0b76ab3d489bccf96ecab99f8
James <james.hinton@live.co.uk>
2023-05-22 16:15:30 +0100

    feat: updated tests to work with new view

--- app/tests/test_stac.py
+++ app/tests/test_stac.py
@@ -1,21 +1,36 @@
-import os
-from dotenv import load_dotenv
-from unittest.mock import Mock, patch
+import pytest
+from fastapi.testclient import TestClient
 
 from app.main import app
-from app.core.db.session import Base
+from app.stac.models import GenerateSTACPayload
+from app.stac.services.stac_item_creator import STACItemCreator
 
-load_dotenv(".env")
-
-client = test_client()
-ENDPOINT = "/stac/"
+client = TestClient(app)
+STATUS_ENDPOINT = "/status"
 
 
-def test_get_stac():
+def test_status():
 
     """
-    Tests if the stac get request is successfull
+    Tests if the stac get request is successful
     """
 
-    response = client.get(ENDPOINT)
+    response = client.get(STATUS_ENDPOINT)
     assert response.status_code == 200
+
+
+def test_create_item():
+    mock_item_dict = {
+        "source": "manual",
+        "provider": "Spatial Days",
+        "collection": "test-collection",
+        "itemId": "test-item",
+        "assetPaths": ["data/Maxar/test-item.tif"],
+        "metadataPaths": ["data/Maxar/metadata.json"],
+    }
+    item = GenerateSTACPayload(**mock_item_dict)
+
+    stac_item_creator = STACItemCreator(item.dict())
+    stac_item = stac_item_creator.create_item()
+
+    assert stac_item.id == "test-item"
