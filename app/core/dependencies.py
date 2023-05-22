3f3e763e32c11c3b5187309208468ff3a3a065f6
James <james.hinton@live.co.uk>
2023-05-22 15:54:16 +0100

    feat: removed RQ and implemented redis incoming requests

--- app/core/dependencies.py
+++
@@ -1,6 +0,0 @@
-from typing import Dict
-from fastapi import Depends, FastAPI
-from starlette.requests import Request
-
-def get_redis_queue(request: Request):
-    return request.app.state.redis_queue
--- app/core/redis.py
+++ app/core/redis.py
@@ -1,8 +1,20 @@
 import redis
-from rq import Queue
+from loguru import logger
+import json
+from app.stac.services.stac_item_creator import STACItemCreator
 
-async def create_redis_connection(host: str, port: int, db: int):
+
+def create_redis_connection(host: str, port: int, db: int):
     return redis.Redis(host=host, port=port, db=db)
 
-def create_rq_queue(redis_conn):
-    return Queue(connection=redis_conn)
+
+def redis_listener(redis_conn, app):
+    while True:
+        item = redis_conn.blpop(app.state.redis_queue_key, timeout=1)
+        if item:
+            logger.info(f"Received item from Redis: {item}")
+            _, job_dict = item
+            item_dict = json.loads(job_dict)
+            stac = STACItemCreator(item_dict).create_item()
+            # rpush the stac item to redis
+            redis_conn.rpush("stac_generator_stac", json.dumps(stac))
