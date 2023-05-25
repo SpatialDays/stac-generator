9217439e55a2b80d3f85a9b19497937524f2955d
James <james.hinton@live.co.uk>
2023-05-25 14:30:40 +0100

    feat: added docs and error handling to view controller

--- app/stac/views.py
+++ app/stac/views.py
@@ -1,13 +1,28 @@
-from fastapi import Depends
-from fastapi import APIRouter
+from fastapi import APIRouter, Depends, HTTPException
 from .models import GenerateSTACPayload
-
 from .services.stac_item_creator import STACItemCreator
 
 router = APIRouter()
 
-
 @router.post("/stac/generate")
-async def generate_stac(item: GenerateSTACPayload):
-    stac = STACItemCreator(item_dict=item.dict()).create_item()
-    return {"stac": stac}
+async def generate_stac(item: GenerateSTACPayload = Depends()):
+    """
+    Generate a STAC (SpatioTemporal Asset Catalog) item from the provided payload.
+
+    This endpoint receives a POST request containing data for a STAC item generation.
+    The payload (item) is passed to the STACItemCreator service which handles the creation 
+    of the STAC item.
+
+    Args:
+        item (GenerateSTACPayload): The payload received from the POST request.
+
+    Returns:
+        GenerateSTACPayload: The created STAC item.
+
+    Raises:
+        HTTPException: If the STAC item creation fails.
+    """
+    try:
+        return STACItemCreator(item.dict()).create_item()
+    except Exception as e:
+        raise HTTPException(status_code=500, detail=str(e))
