f35c70b0c6a0b3fd403714dc61c862483a64e364
James <james.hinton@live.co.uk>
2023-06-08 17:04:05 +0100

    feat: added parser to STAC payload

--- app/stac/models.py
+++ app/stac/models.py
@@ -4,5 +4,6 @@ from pydantic import BaseModel, HttpUrl
 
 class GenerateSTACPayload(BaseModel):
     files: List[str]
-    metadata: Optional[List[HttpUrl]]
+    metadata: Optional[dict]
     method: Optional[str] = "POST"
+    parser: Optional[str] = "example"
