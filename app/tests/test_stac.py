718020de306bae0e07689a7f1fb3c845bfd48675
James <james.hinton@live.co.uk>
2023-06-12 13:52:04 +0100

    test: consolidated some unneeded lines in tests

--- app/tests/test_stac.py
+++ app/tests/test_stac.py
@@ -1,4 +1,4 @@
-# poetry run python -m pytest --log-level=INFO --capture=no
+# `poetry run python -m pytest --log-level=INFO --capture=no` or if in docker do `docker exec -it 8d poetry run poetry run python -m pytest --log-level=INFO --capture=no`
 
 from fastapi.testclient import TestClient
 
@@ -22,9 +22,6 @@ def test_status():
     """
     Tests if the status request is successful
     """
-
-    logger.info("Testing status endpoint")
-
     response = client.get(STATUS_ROUTE)
     assert response.status_code == 200
 
@@ -38,16 +35,13 @@ def test_create_item():
             "https://path-to-cloud-storage.com/readme.md",
             "https://path-to-cloud-storage.com/license.txt",
             "https://path-to-cloud-storage.com/shapefile.shp",
-            "manual-upload-storage-blob/017078204010_01_20AUG12110524-S3DS-017078204010_01_P001.TIF",
+            "manual-upload-storage-blob/017078204010_01_20AUG12110524-S3DS-017078204010_01_P001.TIF",  # This should be your own file
         ],
         "metadata": {
             "ID": "017078204010_01_20AUG12110524-S3DS-017078204010_01_P001",
         },
         "parser": "example",
     }
-
-    logger.info("Testing create item endpoint")
-
     response = client.post(STAC_ROUTE + GENERATE_STAC_ENDPOINT, json=mock_item_dict)
     assert response.status_code == 200
 
@@ -56,10 +50,6 @@ def test_redis_create_item():
     """
     Tests if the item can be added to a Redis list
     """
-
-    logger.info("Testing Redis rpush")
-
-    # Connect to your Redis server (you may need to specify host and port if different than defaults)
     redis_client = redis.StrictRedis(
         host=os.getenv("REDIS_HOST"),
         port=os.getenv("REDIS_PORT"),
@@ -72,7 +62,7 @@ def test_redis_create_item():
     mock_item_dict = {
         "files": [
             "https://somestorageaccount.blob.core.windows.net/somecontainer/folder_path/license.txt",
-            "manual-upload-storage-blob/017078204010_01_20AUG12110524-S3DS-017078204010_01_P001.TIF",
+            "manual-upload-storage-blob/017078204010_01_20AUG12110524-S3DS-017078204010_01_P001.TIF",  # This should be your own file
         ],
         "metadata": {
             "ID": "017078204010_01_20AUG12110524-S3DS-017078204010_01_P001",
@@ -80,10 +70,6 @@ def test_redis_create_item():
         "parser": "example",
     }
 
-    # Convert the dictionary to a JSON string
     payload = json.dumps(mock_item_dict)
-
-    # Use rpush to add the JSON string to the Redis list
     redis_client.rpush(key, payload)
-
     logger.info("Item successfully added to Redis")
