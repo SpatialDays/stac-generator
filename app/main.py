2005a6ce397413b24b10bbdfb1aaf165c6d8f6c0
Ivica <ivica-matic@outlook.com>
2023-07-04 10:27:05 +0100

    fix: moved redis listener in separate entrypoint

--- app/main.py
+++ app/main.py
@@ -1,12 +1,9 @@
 from fastapi import FastAPI, APIRouter
 from dotenv import load_dotenv
-from os import getenv
-import threading
 from loguru import logger
 
 from app.stac import router as stac_router
 from app.core.main_router import router as main_router
-from app.core.redis import create_redis_connection, redis_listener
 from app.core.logger import init_logging
 
 from app.stac.services.metadata_parsers.metadata_parser_manager import (
@@ -22,27 +19,6 @@ app = FastAPI(title="STAC Generator API", version="0.1.0")
 
 @app.on_event("startup")
 def startup_event():
-    try:
-        app.state.redis_conn = create_redis_connection()
-        app.state.redis_conn.ping()
-
-        app.state.redis_queue_key = getenv("REDIS_INPUT_LIST_NAME")
-
-        if app.state.redis_conn:
-            logger.info("Connected to Redis")
-            threading.Thread(
-                target=redis_listener, args=(app.state.redis_conn, app), daemon=True
-            ).start()
-        else:
-            logger.warning(
-                "Could not establish Redis connection, running without Redis."
-            )
-    except Exception as e:
-        logger.error(
-            f"Exception occurred while trying to establish Redis connection: {e}"
-        )
-        logger.warning("Running without Redis.")
-
     MetadataParserManager.list_available_parsers()
 
 
