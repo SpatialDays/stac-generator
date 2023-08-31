9581078917fb68af900948599978797801aa793a
James <james.hinton@live.co.uk>
2023-08-31 14:11:27 +0100

    refactor: remove unneeded comments

--- app/stac/services/metadata_parsers/standard/maxar_parser.py
+++ app/stac/services/metadata_parsers/standard/maxar_parser.py
@@ -24,7 +24,7 @@ class Parser:
         )
         if not metadata_file:
             logger.error("Metadata file not found in the provided payload.")
-            return stac_item  # or you can handle the error differently
+            return stac_item
 
         res = requests.get(metadata_file)
         metadata = ET.fromstring(res.content)
@@ -34,26 +34,14 @@ class Parser:
 
         if product_element is None:
             logger.error("Product element not found.")
-            return stac_item  # or you can handle the error differently
+            return stac_item
 
         # Define the XML elements to extract and their desired names in the output
         elements_to_extract = {
-            "cloudCover": {
-                "name": "eo:cloud_cover",
-                "parser": float
-            },
-            "earliestAcquisitionTime": {
-                "name": "datetime",
-                "parser": str 
-            },
-            "sunElevation": {
-                "name": "view:sun_elevation",
-                "parser": float
-            },
-            "sunAzimuth": {
-                "name": "view:sun_azimuth",
-                "parser": float
-            }
+            "cloudCover": {"name": "eo:cloud_cover", "parser": float},
+            "earliestAcquisitionTime": {"name": "datetime", "parser": str},
+            "sunElevation": {"name": "view:sun_elevation", "parser": float},
+            "sunAzimuth": {"name": "view:sun_azimuth", "parser": float},
         }
 
         for xml_name, props in elements_to_extract.items():
@@ -62,6 +50,5 @@ class Parser:
                 stac_item["properties"][props["name"]] = props["parser"](element.text)
             else:
                 logger.error(f"{xml_name} element not found within product.")
-
 
         return stac_item
